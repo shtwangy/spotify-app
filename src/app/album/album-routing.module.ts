04105b6847164c535a5c96cd2ecd1db2fba28702
shtwangy <shtwangy@gmail.com>
2020-08-30 00:59:15 +0900

    feat(src/app): add album detail routing.
    Issues #4

--- src/app/album/album-routing.module.ts
+++ src/app/album/album-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'detail/:id',
+    component: AlbumDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
