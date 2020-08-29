96d9b2e4ae61d5b7de1abd8440b21347dcff264b
shtwangy <shtwangy@gmail.com>
2020-08-30 02:04:35 +0900

    feat(src/app/album/album-detail): display album detail.
    Closes #4

--- src/app/album/album-detail/album-detail.component.ts
+++ src/app/album/album-detail/album-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { SpotifyService } from '../../services/spotify/spotify.service';
+import { Album } from '../../services/spotify/album';
+import {of} from 'rxjs';
+import {map, switchMap, takeUntil, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-album-detail',
@@ -6,10 +13,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-detail.component.scss']
 })
 export class AlbumDetailComponent implements OnInit {
+  album: Album;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private spotifyService: SpotifyService
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.spotifyService.checkAuth().pipe(
+      switchMap(res => this.spotifyService.getAlbum(id))
+    ).subscribe(
+      res => this.album = res
+    );
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/album/album.module.ts
+++ src/app/album/album.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+
 import { AlbumRoutingModule } from './album-routing.module';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
-
 
 @NgModule({
   declarations: [AlbumDetailComponent],
   imports: [
     CommonModule,
-    AlbumRoutingModule
+    AlbumRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class AlbumModule { }
