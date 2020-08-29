bfcf70a82262b849313fdaf316a8c5ed7e43e04d
shtwangy <shtwangy@gmail.com>
2020-08-30 02:27:18 +0900

    feat(src/app/artist/artist-routing.module): add album detail routing.
    Issues #11

--- src/app/artist/artist-routing.module.ts
+++ src/app/artist/artist-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArtistDetailComponent } from './artist-detail/artist-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'detail/:id',
+    component: ArtistDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
