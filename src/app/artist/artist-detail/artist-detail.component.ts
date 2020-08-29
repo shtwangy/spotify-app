d388eb6542b0a53b63ca879da3980694997e1cf6
shtwangy <shtwangy@gmail.com>
2020-08-30 02:45:40 +0900

    feat(src/app/artist/artist-detail/artist-detail.component): display artist detail.
    Closes #11

--- src/app/artist/artist-detail/artist-detail.component.ts
+++ src/app/artist/artist-detail/artist-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { SpotifyService } from '../../services/spotify/spotify.service';
+
+import { Artist } from '../../services/spotify/artist';
 
 @Component({
   selector: 'app-artist-detail',
@@ -6,10 +12,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-detail.component.scss']
 })
 export class ArtistDetailComponent implements OnInit {
+  artist: Artist;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private spotifyService: SpotifyService
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.spotifyService.checkAuth().pipe(
+      switchMap(res => this.spotifyService.getArtist(id))
+    ).subscribe(
+      res => this.artist = res
+    );
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/artist/artist.module.ts
+++ src/app/artist/artist.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule, MatCardModule } from '@angular/material';
+
 import { ArtistRoutingModule } from './artist-routing.module';
 import { ArtistDetailComponent } from './artist-detail/artist-detail.component';
-
 
 @NgModule({
   declarations: [ArtistDetailComponent],
   imports: [
     CommonModule,
-    ArtistRoutingModule
+    ArtistRoutingModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class ArtistModule { }
