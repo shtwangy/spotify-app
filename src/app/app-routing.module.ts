1fd180797bb8c88c5196fdcec6bf97fa90fdf54c
shtwangy <shtwangy@gmail.com>
2020-08-30 19:09:25 +0900

    feat(src/app/app-routing.module): setting spotify auth guard for each routing.
    Closes #14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SpotifyAuthGuard } from './core/guard/spotify-auth.guard';
 
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then( m => m.HomeModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomeModule),
+    canActivate: [SpotifyAuthGuard]
   },
   {
     path: 'search',
-    loadChildren: () => import('./search/search.module').then( m => m.SearchModule)
+    loadChildren: () => import('./search/search.module').then( m => m.SearchModule),
+    canActivate: [SpotifyAuthGuard]
   },
   {
     path: 'album',
-    loadChildren: () => import('./album/album.module').then( m => m.AlbumModule)
+    loadChildren: () => import('./album/album.module').then( m => m.AlbumModule),
+    canActivate: [SpotifyAuthGuard]
   },
   {
     path: 'artist',
-    loadChildren: () => import('./artist/artist.module').then( m => m.ArtistModule)
+    loadChildren: () => import('./artist/artist.module').then( m => m.ArtistModule),
+    canActivate: [SpotifyAuthGuard]
   },
   {
     path: 'playlist',
-    loadChildren: () => import('./playlist/playlist.module').then( m => m.PlaylistModule)
+    loadChildren: () => import('./playlist/playlist.module').then( m => m.PlaylistModule),
+    canActivate: [SpotifyAuthGuard]
   }
 ];
 
