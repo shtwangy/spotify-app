e8b68d3bd91a69d7b6301b00f7b7b04ff9999463
shtwangy <shtwangy@gmail.com>
2020-08-30 19:08:17 +0900

    feat(src/app/core/guard/spotify-auth.guard): create spotify auth guard.
    Issues #14

---
+++ src/app/core/guard/spotify-auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { SpotifyService } from '../../services/spotify/spotify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyAuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    private spotifyService: SpotifyService
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.spotifyService.checkAuth()
+      .pipe(
+        tap(res => {
+          if ( !res ) {
+            // TODO: 404ページとかに遷移
+            window.location.href = 'https://www.spotify.com/jp/';
+          }
+        })
+      );
+  }
+}
