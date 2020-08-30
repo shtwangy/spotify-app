f888fcb0c7fec9f170fb0ac340270428d43d92d2
shtwangy <shtwangy@gmail.com>
2020-08-30 15:26:32 +0900

    feat(src/app/playlist/playlist-routing.module): add playlist detail routing.
    Issues #13

--- src/app/playlist/playlist-routing.module.ts
+++ src/app/playlist/playlist-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlaylistDetailComponent } from './playlist-detail/playlist-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'detail/:id',
+    component: PlaylistDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
